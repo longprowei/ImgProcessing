1fe3e49c419811e8def6574c0a1a9842c475f206
Chenglong Wei <longprowei@gmail.com>
2019-05-16 17:40:22 +1000

    add error processing and improve code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+'use strict';
+
 const sharp = require('sharp');
 const fs = require("fs");
-
 const imagesDir = "images";
 const distDir = "dist";
+const supportedExts = ['jpeg', 'jpg', 'png', 'webp', 'tiff', 'gif', 'svg'];
 
 // create target directory
 if (!fs.existsSync(distDir)) {
@@ -18,28 +20,50 @@ fs.readdirSync(imagesDir).forEach(file => {
     }
 });
 
-let content = fs.readFileSync("image-config.json");
-let jsonContent = JSON.parse(content);
+let jsonContent = "";
+try {
+    let content = fs.readFileSync("image-config.json");
+    jsonContent = JSON.parse(content);
+    if (!jsonContent || !jsonContent.targets || jsonContent.targets.length === 0) {
+        console.log('config file has issue, please check!');
+        return;
+    }
+} catch(e) {
+    console.log('config file does not exist or has issue, please check');
+    return;
+}
 
-for (let i of allImages) {
-    let pos = i.lastIndexOf('.');
-    let name = i.substr(0, pos);
-    let ext = i.substr(pos + 1);
-    for (let target of jsonContent.targets) {
+for (let i = 0; i < allImages.length; i++) {
+    let fullName = allImages[i];
+    let pos = fullName.lastIndexOf('.');
+    if (pos <= 0 || pos === fullName.length - 1) {
+        console.error(`<${fullName}> is not a correct file name!`);
+        continue;
+    }
+
+    let name = fullName.substring(0, pos);
+    let ext = fullName.substring(pos + 1);
+
+    if (supportedExts.indexOf(ext.toLowerCase()) === -1) {
+        console.error(`<${ext}> file is not supported!`);
+        continue;
+    }
+
+    jsonContent.targets.forEach(target => {
         // use suffix to create subfolder
         let suffixFolder = distDir + `/${target.suffix}`;
         if (!fs.existsSync(suffixFolder)) {
             fs.mkdirSync(suffixFolder);
         }
         let imageName = `${suffixFolder}/${name}${target.noSuffixOnImage ? "" : "-" + target.suffix}.${ext}`
-        sharp(`images/${i}`).resize(parseInt(target.width)).toFile(imageName, (error) => {
+        sharp(`images/${fullName}`).resize(parseInt(target.width)).toFile(imageName, (error) => {
             if (error) {
                 console.log('Errors:');
                 console.log(error);
                 return;
             }
         });
-    }
+    });
 }
 
 console.log('==================Finished!========================');
